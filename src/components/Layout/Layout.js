5cfd2fed3ec6e6068d78507c7118d1bfe1c7b97d
Zana Shala <zanashala2015@gmail.com>
2020-11-26 01:23:24 +0100

    added form-input component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,6 +6,7 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Modal from '../UI/Modal/Modal';
 import DownloadModalContent from '../ModalContent/DownloadModalContent/DownloadModalContent';
+import AddModalContent from '../ModalContent/AddModalContent/AddModalContent';
 
 class Layout extends Component {
     state = {
@@ -28,26 +29,27 @@ class Layout extends Component {
     }
 
     showAddModalHandler = () => {
-        this.setState({showAddModal: true })
+        this.setState({ showAddModal: true })
     }
 
     showDownloadModalHandler = () => {
-        this.setState({showDownloadModal: true })
+        this.setState({ showDownloadModal: true })
     }
 
     render() {
         return (
             <Auxiliary>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} downloadModalClicked={this.showDownloadModalHandler} addModalClicked={this.showAddModalHandler}/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} downloadModalClicked={this.showDownloadModalHandler} addModalClicked={this.showAddModalHandler} />
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
-                <Modal show={this.state.showDownloadModal} modalClosed={this.modalClosedHandler}>
-                    <DownloadModalContent closed={this.modalClosedHandler}/>
-                </Modal>
-                <Modal show={this.state.showAddModal} modalClosed={this.modalClosedHandler}>
-                </Modal>
+                    <Modal show={this.state.showDownloadModal} modalClosed={this.modalClosedHandler}>
+                        <DownloadModalContent closed={this.modalClosedHandler} />
+                    </Modal>
+                    <Modal show={this.state.showAddModal} modalClosed={this.modalClosedHandler}>
+                        <AddModalContent closed={this.modalClosedHandler} />
+                    </Modal>
                 </main>
             </Auxiliary>
         )
---
+++ src/components/ModalContent/AddModalContent/AddModalContent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import classes from './AddModalContent.module.css';
+import CloseBtn from '../../../assets/images/cancel.png';
+import FormInput from '../../Form-Input/Form-Input';
+import Button from '../../UI/Button/Button';
+
+class ModalContent extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          name: '',
+          location: '',
+          date: ''
+        };
+      }
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+   render(){
+    return (
+        <div className={classes.Modal}>
+            <img className={classes.Cancel} src={CloseBtn} alt='Cancel' onClick={this.props.closed} />
+            <FormInput 
+            name='name'
+            type='text'
+            value={this.state.name}
+            handleChange={this.handleChange}
+            label='Emri i raportuesit'
+            required/>
+             <FormInput
+            name='location'
+            type='text'
+            value={this.state.location}
+            handleChange={this.handleChange}
+            label='Lokacioni i aksidentit'
+            required
+          />
+            <FormInput
+            name='data'
+            type='date'
+            value={this.state.date}
+            handleChange={this.handleChange}
+            label='Data e aksidentit'
+            required
+          />
+          <Button btnType='Success'>Shto Aksidentin</Button>
+        </div>
+    )
+}
+}
+export default ModalContent;
