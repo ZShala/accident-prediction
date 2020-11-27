16f77b4ee14342d2c145004212ac182a5f2909c3
Zana Shala <zanashala2015@gmail.com>
2020-11-27 11:38:03 +0100

    changed logo icon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import burgerLogo from '../../assets/images/logo.png';
+import logoIcon from '../../assets/images/logo.png';
 import classes from './Logo.module.css';
-
+import emblem from '../../assets/images/emblem.png';
 const logo = (props) => (
     <div className={classes.Logo} style={{ height: props.height }}>
-        <img src={burgerLogo} alt="MyBurger" />
+    <img src={emblem} alt="emblem" />
+    <img src={logoIcon} alt="logo" />
     </div>
 );
 
